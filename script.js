d99c1daa69eb9f3b99eb4d08777f9ebef4caded1
Tushar Vashisht <tusharvashisht9905@gmail.com>
2024-10-05 21:12:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const images = document.querySelectorAll('.slider img');
+let currentIndex = 0;
+
+function showImage(index) {
+  images.forEach((img, i) => {
+    img.classList.remove('active');
+    if (i === index) {
+      img.classList.add('active');
+    }
+  });
+}
+
+function nextImage() {
+  currentIndex = (currentIndex + 1) % images.length;
+  showImage(currentIndex);
+}
+
+function prevImage() {
+  currentIndex = (currentIndex - 1 + images.length) % images.length;
+  showImage(currentIndex);
+}
